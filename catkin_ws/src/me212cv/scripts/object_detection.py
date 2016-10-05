eaa715b29e7cac51ff6cd53d1d6e134c611a91f7
Peter Yu <peterkty@gmail.com>
2016-10-05 18:47:44 -0400

    improved version

--- catkin_ws/src/me212cv/scripts/object_detection.py
+++ catkin_ws/src/me212cv/scripts/object_detection.py
@@ -4,14 +4,8 @@
 # Peter Yu Oct 2016
 
 import rospy
-import tf
 import numpy as np
-import threading
-import serial
-import pdb
-import traceback
-import sys
-import cv2
+import cv2  # OpenCV module
 
 from sensor_msgs.msg import Image, CameraInfo
 from visualization_msgs.msg import Marker
@@ -21,79 +15,183 @@ from std_msgs.msg import ColorRGBA
 from cv_bridge import CvBridge, CvBridgeError
 import message_filters
 import math
-import matplotlib.pyplot as plt
 
 rospy.init_node('object_detection', anonymous=True)
-vis_pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
-cv_bridge = CvBridge()
 
+# Publisher for publishing pyramid marker in rviz
+vis_pub = rospy.Publisher('visualization_marker', Marker, queue_size=10) 
+
+# Bridge to convert ROS Image type to OpenCV Image type
+cv_bridge = CvBridge()  
+
+# Get the camera calibration parameter for the rectified image
 msg = rospy.wait_for_message('/camera/rgb/camera_info', CameraInfo, timeout=None) 
-#     [fx  0 cx]
-# K = [ 0 fy cy]
-#     [ 0  0  1]
+#     [fx'  0  cx' Tx]
+# P = [ 0  fy' cy' Ty]
+#     [ 0   0   1   0]
 
-fx = msg.K[0]
-fy = msg.K[4]
-cx = msg.K[2]
-cy = msg.K[5]
+fx = msg.P[0]
+fy = msg.P[5]
+cx = msg.P[2]
+cy = msg.P[6]
 
 def main():
-    
-    useHSV = False  
+    useHSV   = False
     useDepth = False
     if not useHSV:
-        # init a cv window
-        cv2.namedWindow("image")
-        # set callback func for cv window
-        cv2.setMouseCallback("image", cvWindowMouseCallBackFunc)
+        # Task 1
         
-        # subscribe to image
-        rospy.Subscriber('/camera/rgb/image_color', Image, rosImageVizCallback)
+        # 1. initialize an OpenCV window
+        cv2.namedWindow("OpenCV_View")
+        
+        # 2. set callback func for mouse hover event
+        cv2.setMouseCallback("OpenCV_View", cvWindowMouseCallBackFunc)
+        
+        # 3. subscribe to image
+        rospy.Subscriber('/camera/rgb/image_rect_color', Image, rosImageVizCallback)
     else:
         if not useDepth:
-            # use HSV color conversion to detect the object
-            rospy.Subscriber('/camera/rgb/image_color', Image, rosHSVImageVizCallBack)
+            # Task 2 Detect object using HSV
+            #    Subscribe to RGB images
+            rospy.Subscriber('/camera/rgb/image_rect_color', Image, rosHSVProcessCallBack)
         else:
-            # use both RGB and Depth image
-            image_sub = message_filters.Subscriber("camera/rgb/image_color", Image)
+            # Task 3: Use Kinect depth data
+            #    Subscribe to both RGB and Depth images with a Synchronizer
+            image_sub = message_filters.Subscriber("camera/rgb/image_rect_color", Image)
             depth_sub = message_filters.Subscriber("camera/depth_registered/image", Image)
 
             ts = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub], 10, 0.5)
             ts.registerCallback(rosRGBDCallBack)
-            
+
     rospy.spin()
+
+# Task 1 callback for ROS image
+def rosImageVizCallback(msg):
+    # 1. convert ROS image to opencv format
+    try:
+        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    # 2. visualize it in a cv window
+    cv2.imshow("OpenCV_View", cv_image)
+    cv2.waitKey(3)
+
+# Task 1 callback for mouse event
+def cvWindowMouseCallBackFunc(event, xp, yp, flags, param):
+    print 'In cvWindowMouseCallBackFunc: (xp, yp)=', xp, yp  # xp, yp is the mouse location in the window
+    # 1. Set the object to 2 meters away from camera
+    zc = 2.0
+    # 2. Visualize the pyramid
+    showPyramid(xp, yp, zc, 10, 10)
+
+# Task 2 callback
+def rosHSVProcessCallBack(msg):
+    try:
+        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+        
+    contours, mask_image = HSVObjectDetection(cv_image)
     
+    for cnt in contours:
+        # Find a bounding box of detected region
+        #  xp, yp are the coordinate of the top left corner of the bounding rectangle
+        #  w, h are the width and height of the bounding rectangle
+        xp,yp,w,h = cv2.boundingRect(cnt)  
+        
+        # Set the object to 2 meters away from camera
+        zc = 2    
+        
+        # Draw the bounding rectangle
+        cv2.rectangle(cv_image,(xp,yp),(xp+w,yp+h),[0,255,255], 2)
+        
+        centerx, centery = xp+w/2, yp+h/2
+        showPyramid(centerx, centery, zc, w, h)
     
+    showImageInCVWindow(cv_image, mask_image)
+
+# Task 2 object detection code
+def HSVObjectDetection(cv_image, toPrint = True):
+    # convert image to HSV color space
+    # hsv_image = ??
+    
+    # define range of red color in HSV
+    # lower_red = ??
+    # upper_red = ??
+
+    # Threshold the HSV image to get only red colors
+    # mask = ??
+    
+    # mask_eroded         = ??
+    # mask_eroded_dilated = ??
+    
+    if toPrint:
+        print 'hsv', hsv_image[240][320] # the center point hsv
+
+    contours,hierarchy = cv2.findContours(mask_eroded_dilated,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    return contours, mask
+
+# Task 3 callback
+def rosRGBDCallBack(rgb_data, depth_data):
+    try:
+        cv_image = cv_bridge.imgmsg_to_cv2(rgb_data, "bgr8")
+        cv_depthimage = cv_bridge.imgmsg_to_cv2(depth_data, "32FC1")
+        cv_depthimage2 = np.array(cv_depthimage, dtype=np.float32)
+    except CvBridgeError as e:
+        print(e)
+
+    contours, mask_image = HSVObjectDetection(cv_image, toPrint = False)
+
+    for cnt in contours:
+        xp,yp,w,h = cv2.boundingRect(cnt)
+        
+        # Get depth value from depth image, need to make sure the value is in the normal range 0.1-10 meter
+        if not math.isnan(cv_depthimage2[int(yp)][int(xp)]) and cv_depthimage2[int(yp)][int(xp)] > 0.1 and cv_depthimage2[int(yp)][int(xp)] < 10.0:
+            zc = cv_depthimage2[int(yp)][int(xp)]
+            print 'zc', zc
+        else:
+            continue
+            
+        centerx, centery = xp+w/2, yp+h/2
+        cv2.rectangle(cv_image,(xp,yp),(xp+w,yp+h),[0,255,255],2)
+        
+        showPyramid(centerx, centery, zc, w, h)
+
+    showImageInCVWindow(cv_image, mask_image)
+
 def getXYZ(xp, yp, zc, fx,fy,cx,cy):
-    ## 
-    ## xn = ??
-    ## yn = ??
-    ## xc = ??
-    ## yc = ??
+    # xn = ??
+    # yn = ??
+    # xc = ??
+    # yc = ??
     return (xc,yc,zc)
+
+def showImageInCVWindow(cv_image, mask_image):
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(cv_image, cv_image, mask = mask_image)
     
+    # Draw a cross at the center of the image
+    cv2.line(cv_image, (320, 235), (320, 245), (255,0,0))
+    cv2.line(cv_image, (325, 240), (315, 240), (255,0,0))
     
+    # Show the images
+    cv2.imshow('OpenCV_Original', cv_image)
+    cv2.imshow('OpenCV_Mask', mask_image)
+    cv2.imshow('OpenCV_View', res)
+    cv2.waitKey(3)
+
+# Create a pyramid using 4 triangles
 def showPyramid(xp, yp, zc, w, h):
+    # X1-X4 are the 4 corner points of the base of the pyramid
     X1 = getXYZ(xp-w/2, yp-h/2, zc, fx, fy, cx, cy)
     X2 = getXYZ(xp-w/2, yp+h/2, zc, fx, fy, cx, cy)
     X3 = getXYZ(xp+w/2, yp+h/2, zc, fx, fy, cx, cy)
     X4 = getXYZ(xp+w/2, yp-h/2, zc, fx, fy, cx, cy)
-    vis_pub.publish(createConeMarker(1, [X1,X2,X3,X4], [1,0,0,1], frame_id = '/camera'))
+    vis_pub.publish(createTriangleListMarker(1, [X1, X2, X3, X4], rgba = [1,0,0,1], frame_id = '/camera'))
 
-def depthImageCallBack(msg):
-    # 1. convert ROS image to opencv format
-    try:
-        cv_image = cv_bridge.imgmsg_to_cv2(msg, "32FC1")
-    except CvBridgeError as e:
-        print(e)
-    
-def cvWindowMouseCallBackFunc(event, xp, yp, flags, param):
-    print 'In cvWindowMouseCallBackFunc: (xp, yp)=', xp, yp 
-    zc = 2.0
-    # visualize the pyramid
-    showPyramid(xp, yp, zc, 10, 10)
-
-def createConeMarker(marker_id, points, rgba, frame_id = '/camera'):
+# Create a list of Triangle markers for visualization
+def createTriangleListMarker(marker_id, points, rgba, frame_id = '/camera'):
     marker = Marker()
     marker.header.frame_id = frame_id
     marker.type = marker.TRIANGLE_LIST
@@ -105,7 +203,6 @@ def createConeMarker(marker_id, points, rgba, frame_id = '/camera'):
     if rgba is not None:
         marker.color = ColorRGBA(*rgba)
         
-    
     o = Point(0,0,0)
     for i in xrange(n):
         p = Point(*points[i])
@@ -120,106 +217,6 @@ def createConeMarker(marker_id, points, rgba, frame_id = '/camera'):
 def poselist2pose(poselist):
     return Pose(Point(*poselist[0:3]), Quaternion(*poselist[3:7]))
 
-def rosImageVizCallback(msg):
-    # 1. convert ROS image to opencv format
-    try:
-        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-    except CvBridgeError as e:
-        print(e)
-
-    # 2. visualize it in a cv window
-    cv2.imshow("image", cv_image)
-    cv2.waitKey(3)
-
-    
-def rosHSVImageVizCallBack(msg):
-    try:
-        cv_image = cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-    except CvBridgeError as e:
-        print(e)
-        
-    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    
-    # define range of blue color in HSV (Change the thresholds here)
-    lower_red = np.array([170,50,50])
-    upper_red = np.array([180,255,255])
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    
-    mask_eroded         = cv2.erode(mask, None,iterations = 3)
-    mask_eroded_dilated = cv2.dilate(mask_eroded, None,iterations = 10)
-    
-    print 'hsv', hsv[240][320]   # this printout is useful to find out the threshold
-
-    # Find the blobs in the mask and get their (xp,yp)
-    contours,hierarchy = cv2.findContours(mask_eroded_dilated,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        xp,yp,w,h = cv2.boundingRect(cnt)
-        zc = 2 # guess
-        centerx,centery = xp+w/2, yp+h/2
-        cv2.rectangle(cv_image,(xp,yp),(xp+w,yp+h),[0,255,255],2)
-        
-        showPyramid(centerx, centery, zc, w, h)
-    
-
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(cv_image,cv_image, mask= mask_eroded_dilated)
-    cv2.line(cv_image, (320, 235), (320, 245), (255,0,0))
-    cv2.line(cv_image, (325, 240), (315, 240), (255,0,0))
-    cv2.imshow('image',cv_image)
-    cv2.imshow('mask',mask_eroded_dilated)
-    cv2.imshow('res',res)
-    cv2.waitKey(3)
-
-def rosRGBDCallBack(rgb_data, depth_data):
-    try:
-        cv_image = cv_bridge.imgmsg_to_cv2(rgb_data, "bgr8")
-        cv_depthimage = cv_bridge.imgmsg_to_cv2(depth_data, "32FC1")
-        cv_depthimage2 = np.array(cv_depthimage, dtype=np.float32)
-    except CvBridgeError as e:
-        print(e)
-        
-    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    
-    # define range of blue color in HSV (Change the thresholds here as in Task 2)
-    lower_red = np.array([170,50,50])
-    upper_red = np.array([180,255,255])
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    
-    mask_eroded         = cv2.erode(mask, None,iterations = 3)
-    mask_eroded_dilated = cv2.dilate(mask_eroded, None,iterations = 10)
-    
-    contours,hierarchy = cv2.findContours(mask_eroded_dilated,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        xp,yp,w,h = cv2.boundingRect(cnt)
-        
-        # get zc from depth image
-        if not math.isnan(cv_depthimage2[int(yp)][int(xp)] ) and cv_depthimage2[int(yp)][int(xp)] > 0.1:
-            zc = cv_depthimage2[int(yp)][int(xp)]
-            print zc
-        else:
-            continue
-            zc = 1
-            
-        print cv_depthimage2[int(yp)][int(xp)]
-        centerx,centery = xp+w/2, yp+h/2
-        cv2.rectangle(cv_image,(xp,yp),(xp+w,yp+h),[0,255,255],2)
-        
-        showPyramid(centerx, centery, zc, w, h)
-            
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(cv_image,cv_image, mask= mask_eroded_dilated)
-    cv2.line(cv_image, (320, 235), (320, 245), (255,0,0))
-    cv2.line(cv_image, (325, 240), (315, 240), (255,0,0))
-    cv2.imshow('image',cv_image)
-    cv2.imshow('mask',mask_eroded_dilated)
-    cv2.imshow('res',res)
-    
-    cv2.waitKey(3)
-    
 if __name__=='__main__':
     main()
     
